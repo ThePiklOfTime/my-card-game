9af663a2f38acb4c4f73451264b222d216f4a808
Nico Tolvanen <nicotolvanen@gmail.com>
2022-09-26 20:50:10 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { json } from "@sveltejs/kit";
+    //@ts-ignore
+    import deck from '$lib/data/cah-cards-full.json';
+	import { each } from "svelte/internal";
+    let official = true;
+    let hello: { [key: string]: boolean }  = {};
+    $: {
+        console.log(hello)
+    }
+</script>
+<div class="cardSelector">
+{#each deck as dick}
+{#if dick["official"] == official}
+<div>
+    <input type="checkbox" bind:checked={hello[dick["name"]]}> 
+    <p>
+        {dick["name"]}
+    </p>
+</div>
+{/if}
+    
+{/each}
+</div>
+<style>
+.cardSelector {
+    display: flex;
+    flex-wrap: wrap;
+    background-color: #362222;
+    padding: 1em;
+    border-radius: 20px;
+
+}
+div{
+    display: flex;
+}
+p {
+    padding: 0.2em;
+    margin: 0.3em;
+    outline: 1px solid black;
+    color: white;
+    border-radius: 5px;
+    font-size: 0.9em;
+    box-shadow: 1px;
+    background-color: #423F3E;
+}
+
+</style>
---
+++ tests/test.ts
@@ -0,0 +1,7 @@
+//@ts-ignore
+import { expect, test } from '@playwright/test';
+//@ts-ignore
+test('index page has expected h1', async ({ page }) => {
+	await page.goto('/');
+	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
+});

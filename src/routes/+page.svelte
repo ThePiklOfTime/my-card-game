ac1e3a4d2114950dcd846a6424b56267a37b24e4
nico <nicotolvanen@gmail.com>
2022-09-27 00:04:48 +0300

    managed database on the page. Only temporary

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,26 @@
     //@ts-ignore
     import deck from '$lib/data/cah-cards-full.json';
 	import { each } from "svelte/internal";
+    import PocketBase from 'pocketbase';
+    let user = import.meta.env.VITE_USERNAME
+    let pass = import.meta.env.VITE_PASS //move this to server side for security reasons
+    let myDick = JSON.stringify(deck[0])
+    const client = new PocketBase('http://127.0.0.1:8090');
+    async function db() {
+        await client.admins.authViaEmail(user, pass)
+        deck.forEach(element => {
+            const record = client.records.create('deck', {field : element, name: element["name"]}).then()
+        });
+
+    }
+    db()
+
     let official = true;
     let hello: { [key: string]: boolean }  = {};
     $: {
         console.log(hello)
     }
+
 </script>
 <div class="cardSelector">
 {#each deck as dick}
